f61ddbfd2810c2d0e7d90172e2194f2072a4de26
Talbekk <mt.campbell@outlook.com>
2019-11-10 21:00:10 +0000

    edit match option added

--- controllers/matches_controller.rb
+++ controllers/matches_controller.rb
@@ -29,24 +29,19 @@ get '/matches/:id' do
   @match = Match.find(params['id'].to_i)
   erb(:"matches/show")
 end
-#
-# get '/matches' do
-#   @match = Match.new(params)
-#   @match.save
-#   erb(:"/matches/create")
-# end
-#
-# get '/matches/:id/edit' do
-#   @trainers = Trainer.all
-#   @match = Match.find(params['id'])
-# end
-#
-# post '/matches/:id' do
-#   match = Match.new(params)
-#   match.update
-#   redirect to '/matches/#{params[id]}'
-# end
-#
+
+get '/matches/:id/edit' do
+  @trainers = Trainer.all
+  @match = Match.find(params['id'])
+  erb(:"matches/edit")
+end
+
+post '/matches/:id' do
+  match = Match.new(params)
+  match.update()
+  redirect to "/matches/#{params['id']}"
+end
+
 post '/matches/:id/delete' do
   match = Match.find(params['id'].to_i)
   match.delete()
