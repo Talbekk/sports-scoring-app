df873e9711116565a036e163468045cf51ad1cc4
Talbekk <mt.campbell@outlook.com>
2019-11-10 13:37:08 +0000

    Setup Webside

--- models/match.rb
+++ models/match.rb
@@ -2,33 +2,33 @@ require_relative( '../db/sql_runner' )
 
 class Match
 
-  attr_reader( :id, :trainer1_id, :home_score, :trainer2_id, :away_score )
+attr_reader( :id, :trainer1_id, :home_score, :trainer2_id, :away_score )
 
-  def initialize( options )
-    @id = options['id'].to_i if options['id']
-    @trainer1_id = options['trainer1_id']
-    @home_score = options['home_score']
-    @trainer2_id = options['trainer2_id']
-    @away_score = options['away_score']
-  end
+def initialize( options )
+  @id = options['id'].to_i if options['id']
+  @trainer1_id = options['trainer1_id']
+  @home_score = options['home_score']
+  @trainer2_id = options['trainer2_id']
+  @away_score = options['away_score']
+end
 
-  def save()
-    sql = "INSERT INTO matches
-    (
-      trainer1_id,
-      home_score,
-      trainer2_id,
-      away_score
-    )
-    VALUES
-    (
-      $1, $2, $3, $4
-    )
-    RETURNING id"
-    values = [@trainer1_id, @home_score, @trainer2_id, @away_score]
-    results = SqlRunner.run(sql, values)
-    @id = results.first()['id'].to_i
-  end
+def save()
+  sql = "INSERT INTO matches
+  (
+    trainer1_id,
+    home_score,
+    trainer2_id,
+    away_score
+  )
+  VALUES
+  (
+    $1, $2, $3, $4
+  )
+  RETURNING id"
+  values = [@trainer1_id, @home_score, @trainer2_id, @away_score]
+  results = SqlRunner.run(sql, values)
+  @id = results.first()['id'].to_i
+end
 
 def trainer1()
   sql = "SELECT * FROM trainers
