15607597788c36b8a4f25ee52b83eb090ccda0ed
Talbekk <mt.campbell@outlook.com>
2019-11-10 14:19:01 +0000

    Seven Restful Routes for Matches

--- controllers/matches_controller.rb
+++ controllers/matches_controller.rb
@@ -9,10 +9,38 @@ also_reload( '../models/*' )
 
 get '/matches' do
   @matches = Match.all()
-  erb ( :"matches/index" )
+  erb (:"matches/index")
+end
+
+get '/matches/new' do
+  @matches = Match.all()
+  erb (:new)
 end
 
 get '/matches/:id' do
   @matches = Match.find(params['id'].to_i)
-  erb( :"matches/show" )
+  erb(:"matches/show")
+end
+
+get '/matches' do
+  @match = Match.new(params)
+  @match.save
+  erb(:"/matches/create")
+end
+
+get '/matches/:id/edit' do
+  @trainers = Trainers.all?
+  @match = Match.find(params['id'])
+end
+
+post '/matches/:id' do
+  match = Match.new(params)
+  match.update
+  redirect to '/matches/#{params[id]}'
+end
+
+post '/matches/:id/delete' do
+  match = Match.find(params[:id])
+  match.delete()
+  redirect to '/matches'
 end
