6ef54b11bfdd6c15e6a9404b80057e5d01a4ee07
Talbekk <mt.campbell@outlook.com>
2019-11-10 16:17:19 +0000

    remove trainer option added

--- controllers/trainers_controller.rb
+++ controllers/trainers_controller.rb
@@ -25,16 +25,10 @@ post '/trainers' do
 end
 
 get '/trainers/:id' do
-  @trainers = Trainer.find(params['id'].to_i)
+  @trainer = Trainer.find(params['id'].to_i)
   erb(:"trainers/show")
 end
-#
-# get '/trainers' do
-#   @trainer = Trainer.new(params)
-#   @trainer.save
-#   erb(:"/trainers/create")
-# end
-#
+
 # get '/trainers/:id/edit' do
 #   @matches = Match.all
 #   @trainer = trainer.find(params['id'])
@@ -43,11 +37,11 @@ end
 # post '/trainers/:id' do
 #   trainer = Trainer.new(params)
 #   trainer.update
-#   redirect to '/trainers/#{params[id]}'
-# end
-#
-# post '/trainers/:id/delete' do
-#   trainer = Trainer.find(params[:id])
-#   trainer.delete()
 #   redirect to '/trainers'
 # end
+#
+post '/trainers/:id/delete' do
+  trainer = Trainer.find(params[:id])
+  trainer.delete()
+  redirect to '/trainers'
+end
