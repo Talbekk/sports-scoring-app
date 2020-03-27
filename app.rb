05096b3271b23471efaca8c5312c8587f187b91c
Talbekk <mt.campbell@outlook.com>
2020-03-27 14:30:10 +0000

    removes also reload from app

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require('sinatra')
-require('sinatra/contrib/all')
+require('sinatra/contrib/all') if development?
 require_relative('controllers/trainers_controller.rb')
 require_relative('controllers/matches_controller.rb')
 require_relative('controllers/monsters_controller.rb')
 
-also_reload( './models/*' )
+# also_reload( './models/*' )
 
 get '/' do
   @trainers1 = Trainer.all()
