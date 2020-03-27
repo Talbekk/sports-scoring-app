2609d03e08aa19370e2b5ad426ff8de1ff4c18ca
Talbekk <mt.campbell@outlook.com>
2020-03-27 16:07:56 +0000

    fixes typo

--- app.rb
+++ app.rb
@@ -3,9 +3,9 @@ require('sinatra/contrib/all') if development?
 require_relative('controllers/trainers_controller.rb')
 require_relative('controllers/matches_controller.rb')
 require_relative('controllers/monsters_controller.rb')
-require_relative('models/trainers.rb')
-require_relative('models/matches.rb')
-require_relative('models/monsters.rb')
+require_relative('models/trainer.rb')
+require_relative('models/match.rb')
+require_relative('models/monster.rb')
 
 # also_reload( './models/*' )
 
