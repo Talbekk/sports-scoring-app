688b9d6f5b363b5b4cdc7ede66af757c49d61fc0
Talbekk <mt.campbell@outlook.com>
2020-03-27 16:10:15 +0000

    adds new fix

--- app.rb
+++ app.rb
@@ -10,11 +10,11 @@ require_relative('models/monster.rb')
 # also_reload( './models/*' )
 
 get '/' do
-  @trainers1 = Trainer.all()
-  @trainers2 = @trainers1.sort_by {|trainer| trainer.points}
-  @trainers = @trainers2.reverse
-  @match1 = Match.all()
-  @match = @match1.last()
+  # @trainers1 = Trainer.all()
+  # @trainers2 = @trainers1.sort_by {|trainer| trainer.points}
+  # @trainers = @trainers2.reverse
+  # @match1 = Match.all()
+  # @match = @match1.last()
   erb( :index )
 end
 
