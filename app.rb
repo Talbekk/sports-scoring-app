c722ac67777013fe07ebbf964d6e6f43266e60ef
Talbekk <mt.campbell@outlook.com>
2020-03-27 16:06:30 +0000

    adds models to app

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require('sinatra/contrib/all') if development?
 require_relative('controllers/trainers_controller.rb')
 require_relative('controllers/matches_controller.rb')
 require_relative('controllers/monsters_controller.rb')
+require_relative('models/trainers.rb')
+require_relative('models/matches.rb')
+require_relative('models/monsters.rb')
 
 # also_reload( './models/*' )
 
