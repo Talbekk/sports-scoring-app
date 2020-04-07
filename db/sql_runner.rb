fa9e2ace9e8b49c12a3cefe541b0b92d77783a5e
Talbekk <mt.campbell@outlook.com>
2020-04-07 11:55:06 +0100

    fixes line indentation

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -15,8 +15,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'd85805srvf09sr', host: 'ec2-50-17-21-170.compute-1.amazonaws.com
-      ', port: 5432, user: 'jwjzcafqnanrcl', password: 'ca79b48474c09c188f381ae74f6e45ba2f912c123c126b99e890a57414faed9d' })
+      db = PG.connect({ dbname: 'd85805srvf09sr', host: 'ec2-50-17-21-170.compute-1.amazonaws.com', port: 5432, user: 'jwjzcafqnanrcl', password: 'ca79b48474c09c188f381ae74f6e45ba2f912c123c126b99e890a57414faed9d' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
